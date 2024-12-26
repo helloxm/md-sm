c6b67f1121936bc2b3e3c26eb93e24660311c238
helloxm <fjt0327@163.com>
2024-12-26 14:27:55 +0800

    update

--- src/components/CodemirrorEditor/EditorHeader/AboutDialog.vue
+++ src/components/CodemirrorEditor/EditorHeader/AboutDialog.vue
@@ -13,16 +13,6 @@ function onUpdate(val: boolean) {
     emit(`close`)
   }
 }
-
-const links = [
-  { label: `GitHub 仓库`, url: `https://github.com/doocs/md` },
-  { label: `Gitee 仓库`, url: `https://gitee.com/doocs/md` },
-  { label: `GitCode 仓库`, url: `https://gitcode.com/doocs/md` },
-]
-
-function onRedirect(url: string) {
-  window.open(url, `_blank`)
-}
 </script>
 
 <template>
@@ -32,24 +22,15 @@ function onRedirect(url: string) {
         <DialogTitle>关于</DialogTitle>
       </DialogHeader>
       <div class="text-center">
-        <h3>一款高度简洁的微信 Markdown 编辑器</h3>
-        <p>扫码关注公众号 Doocs，原创技术文章第一时间推送！</p>
+        <h3>水木-信奥 公众号 Markdown 编辑器</h3>
+        <p>使用上的问题，扫码咨询！</p>
         <img
           class="mx-auto my-5"
-          src="https://cdn-doocs.oss-cn-shenzhen.aliyuncs.com/gh/doocs/md/images/1648303220922-7e14aefa-816e-44c1-8604-ade709ca1c69.png"
-          alt="Doocs Markdown 编辑器"
+          src="https://oj.noicoding.cn/file/2/oDFtQ50DVK-VrSF18vDat.png"
+          alt="水木-信奥 牛老师"
           style="width: 40%"
         >
       </div>
-      <DialogFooter class="sm:justify-evenly">
-        <Button
-          v-for="link in links"
-          :key="link.url"
-          @click="onRedirect(link.url)"
-        >
-          {{ link.label }}
-        </Button>
-      </DialogFooter>
     </DialogContent>
   </Dialog>
 </template>
